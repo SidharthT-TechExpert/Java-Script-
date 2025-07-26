1155ae1cf6330734462303222f4d75bb5ec11baa
Sidharth T <sidhartht900@gmail.com>
2025-07-26 13:41:52 +0530

    Time: 51 ms (51.46%) | Memory: 55.5 MB (48.28%) - LeetSync

--- 7-reverse-integer/reverse-integer.js
+++ 7-reverse-integer/reverse-integer.js
@@ -3,24 +3,19 @@
  * @return {number}
  */
 var reverse = function (x) {
-    // if(  x >= 0 )
-    //   return parseInt(x.toString().split('').reverse().join('')) 
-
-    //  x = - x 
-
-    //  x =  - (parseInt(x.toString().split('').reverse().join(''))) 
-
-    //  return x ;
-//sdfsdf
-    const MIN = -2147483648;
-    const MAX = 2147483647;
-
-    let sign = x < 0 ? -1 : 1;
-    let reversed = parseInt(Math.abs(x).toString().split('').reverse().join('')) * sign;
-
-    if (reversed < MIN || reversed > MAX) {
-        return 0;
+    let reversed;
+    if (x >= 0)
+        reversed = parseInt(x.toString().split('').reverse().join(''))
+    else {
+        reversed = - parseInt(Math.abs(x).toString().split('').reverse().join(''))
     }
 
-    return reversed;
+
+
+
+
+    const MIN = - (2 ** 31)
+    const MAX = (2 ** 31) - 1
+    
+    return (reversed >= MIN && reversed <= MAX) ? reversed : 0;
 };
