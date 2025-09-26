f364e013e45cee24e1f11799233ecc9d2b36e100
Sidharth T <sidhartht900@gmail.com>
2025-09-26 12:51:09 +0530

    Time: 0 ms (100.00%) | Memory: 54.5 MB (61.84%) - LeetSync

--- 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.js
+++ 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.js
@@ -4,5 +4,5 @@
  */
 var arraySign = function (nums) {
     const product = nums.reduce((acc, curr) => acc * curr, 1);
-    return product > 0 ? 1 : product < 0 ? -1 : 0 ;
+    return product > 0 ? 1 : product < 0 ? -1 : 0;
 };
