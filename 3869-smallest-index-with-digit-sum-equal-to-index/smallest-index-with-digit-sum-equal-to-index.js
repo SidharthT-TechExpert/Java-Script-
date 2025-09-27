0dc0e52015e68ddad384d645fd8e849794c55c73
Sidharth T <sidhartht900@gmail.com>
2025-09-27 14:17:11 +0530

    Time: 2 ms (48.47%) | Memory: 54.5 MB (98.16%) - LeetSync

--- 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.js
+++ 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.js
@@ -3,22 +3,19 @@
  * @return {number}
  */
 var smallestIndex = function (nums) {
-
     const digitSum = (n) => {
-        n = Math.abs(n);
-
         let sum = 0;
         while (n > 0) {
             sum += n % 10;
-            n = parseInt(n / 10);
+            n = Math.floor(n / 10)
         }
-
         return sum;
     }
+
     for (let i = 0; i < nums.length; i++) {
-        if (digitSum(nums[i]) === i) {
-            return i;
-        }
+        const value = (digitSum(nums[i]) === i) ? i : null
+        if (value !== null)
+            return value;
     }
     return -1;
 };
