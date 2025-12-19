0cf9e64645bb727975546497dd511a103b67cbb8
Sidharth T <sidhartht900@gmail.com>
2025-12-19 15:40:18 +0530

    Time: 1 ms (29.52%) | Memory: 57.7 MB (6.45%) - LeetSync

--- 231-power-of-two/power-of-two.js
+++ 231-power-of-two/power-of-two.js
@@ -3,14 +3,13 @@
  * @return {boolean}
  */
 var isPowerOfTwo = function (n) {
-    // if (n <= 0) return false;
+    if (n <= 0) return false;
 
-    // let i = 0;
-    // while (2 ** i <= n) {
-    //     if (n == 2 ** i) return true;
-    //     i++;
-    // }
+    let i = 0;
+    while (2 ** i <= n) {
+        if (n == 2 ** i) return true;
+        i++;
+    }
 
-    // return false;
-    return n > 0 && (n & (n - 1)) === 0;
+    return false;
 };
