2bb21806bbed6a4e3abcfd14379a43e6c2502134
Sidharth T <sidhartht900@gmail.com>
2025-12-19 13:53:49 +0530

    Time: 0 ms (100.00%) | Memory: 53.4 MB (88.40%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function (num) {
+    if (num < 2) return true;
+
+    let left = 1, right = num;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        let square = mid * mid;
+
+        if (square === num) return true;
+        if (square < num) left = mid + 1;
+        else right = mid - 1;
+    }
+
+    return false;
+};
