af30ad365e0c1ca898e1bee4bb789ff38d051864
Sidharth T <sidhartht900@gmail.com>
2025-12-23 19:47:36 +0530

    Time: 1 ms (62.39%) | Memory: 54.6 MB (70.38%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var backspaceCompare = function(s, t) {
+  const process = (str) => {
+    let stock = [];
+     for(let ch of str){
+        if(ch === '#'){
+           stock.pop();
+        }else{
+            stock.push(ch)
+        }
+     }
+    return stock.join('');
+  }
+
+  return process(s) === process(t);
+};
