b013d1800afd42dabf047306a446df428ef9aeda
Sidharth T <sidhartht900@gmail.com>
2025-12-23 17:26:24 +0530

    Time: 0 ms (100.00%) | Memory: 56.6 MB (86.72%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -4,15 +4,17 @@
  */
 var majorityElement = function (nums) {
     let freq = {};
+
     nums.forEach(num => {
         freq[num] = (freq[num] || 0) + 1
-    });
+    })
 
-    const majority = nums.length/2;
+    let majority = nums.length / 2;
 
-    for(let keys in freq){
-        if(freq[keys]>majority){
-           return Number(keys);
+    for (let num in freq) {
+        if (freq[num] > majority) {
+            return Number(num)
         }
     }
+
 };
