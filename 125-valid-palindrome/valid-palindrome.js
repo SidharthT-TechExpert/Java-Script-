fc0f3dfdde88043f2c320401ab4cfa6f4fb5248a
Sidharth T <sidhartht900@gmail.com>
2025-12-27 12:16:09 +0530

    Time: 3 ms (85.66%) | Memory: 57.3 MB (77.00%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -3,6 +3,6 @@
  * @return {boolean}
  */
 var isPalindrome = function (s) {
-    let cleaned = s.replace(/[^a-z0-9]/gi, '').toLowerCase();
-    return cleaned === cleaned.split('').reverse().join('');
+   const newS = s.replace(/[^a-z0-9]/gi,'').toLowerCase();
+   return newS == newS.split('').reverse().join('');
 };
