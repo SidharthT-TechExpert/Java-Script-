0c00e0dedd8990ac5061cee932da6b86f4242c0d
Sidharth T <sidhartht900@gmail.com>
2026-02-05 09:57:59 +0530

    Time: 0 ms (100.00%) | Memory: 54 MB (61.88%) - LeetSync

---
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var balancedStringSplit = function (s) {
+    let count = 0;
+    let balance = 0;
+
+    for (let char of s) {
+        if (char === 'L') {
+            balance++;
+        } else {
+            balance--;
+        }
+
+        if (balance) continue;
+        count++;
+    }
+
+    return count;
+};
