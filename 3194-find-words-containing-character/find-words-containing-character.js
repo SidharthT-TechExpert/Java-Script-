f1052838b15e74b431635a6cf93b2ab34d76f13a
Sidharth T <sidhartht900@gmail.com>
2026-02-17 09:15:27 +0530

    Time: 1 ms (82.59%) | Memory: 57.6 MB (68.91%) - LeetSync

---
+++ 3194-find-words-containing-character/find-words-containing-character.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string[]} words
+ * @param {character} x
+ * @return {number[]}
+ */
+var findWordsContaining = function(words, x) {
+    let indexAr = [];
+    words.forEach((word,i) => {
+        if(word.includes(x)) indexAr.push(i)
+    });
+
+    return indexAr;
+};
