3edf3af0dc8b68803d599bd0caacb2dd62ae2e19
Sidharth T <sidhartht900@gmail.com>
2026-03-02 09:34:45 +0530

    Time: 6 ms (66.09%) | Memory: 65.1 MB (29.34%) - LeetSync

--- 9-palindrome-number/palindrome-number.js
+++ 9-palindrome-number/palindrome-number.js
@@ -3,6 +3,5 @@
  * @return {boolean}
  */
 var isPalindrome = function (x) {
-    if (x < 0) return false;
-    return x === Number(x.toString().split('').reverse().join(''));
+   return x < 0 ? false : x === Number(x.toString().split('').reverse().join(''))
 };
