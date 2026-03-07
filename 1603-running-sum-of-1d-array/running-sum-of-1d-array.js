6a0252f17bc2ea4ce1fe662e26f547047f9ccf86
Sidharth T <sidhartht900@gmail.com>
2026-03-07 09:19:58 +0530

    Time: 1 ms (35.57%) | Memory: 56.1 MB (16.43%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.js
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.js
@@ -3,11 +3,11 @@
  * @return {number[]}
  */
 var runningSum = function (nums) {
-    let newArray = [];
-    let sum = 0;
-    nums.forEach((num) => {
-        sum += num;
-        newArray.push(sum)
-    });
-    return newArray;
+    outArray = [nums[0]];
+    for (let i = 1; i < nums.length; i++) {
+        lastIndex = outArray.length - 1;
+        const sum = outArray[lastIndex] + nums[i]
+        outArray.push(sum);
+    }
+    return outArray;
 };
