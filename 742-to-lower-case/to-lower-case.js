f4d9465588cef6a2bcff6a5e048b2a94c138a849
Sidharth T <sidhartht900@gmail.com>
2026-03-07 09:31:53 +0530

    Time: 1 ms (14.68%) | Memory: 54.5 MB (14.06%) - LeetSync

--- 742-to-lower-case/to-lower-case.js
+++ 742-to-lower-case/to-lower-case.js
@@ -3,5 +3,18 @@
  * @return {string}
  */
 var toLowerCase = function (s) {
-    return s.toLowerCase();
+    let output = [];
+    for (let char of s) {
+        if (isUpperCase(char)) {
+            output.push(char.toLowerCase());
+            continue;
+        }
+        output.push(char);
+    }
+    return output.join('')
 };
+
+function isUpperCase(char) {
+    let code = char.charCodeAt(0);
+    return code >= 65 && code <= 90;
+}
