037d37d4ee8708a85814793df06d10c8110250ce
Sidharth T <sidhartht900@gmail.com>
2026-04-05 21:31:59 +0530

    Time: 1 ms (34.67%) | Memory: 54.6 MB (43.33%) - LeetSync

---
+++ 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var sumZero = function (n) {
+    const result = [];
+
+    for (let i = 1; i <= Math.floor(n / 2); i++) {
+        result.push(i);
+        result.push(-i);
+    }
+
+    if (n % 2 !== 0) {
+        result.push(0);
+    }
+
+    return result;
+};
