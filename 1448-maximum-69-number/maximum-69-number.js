47c35715c247f2d750e0e56ae9f48b1999eb61eb
Sidharth T <sidhartht900@gmail.com>
2026-04-21 11:56:55 +0530

    Time: 2 ms (2.59%) | Memory: 54.3 MB (12.44%) - LeetSync

---
+++ 1448-maximum-69-number/maximum-69-number.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var maximum69Number = function (num) {
+    let maxValue = [];
+    const newArray = num.toString().split('');
+
+    newArray.map((n, i) => {
+        if (n !== '9') {
+            const array = [...newArray];
+            array.splice(i, 1, 9);
+            maxValue.push(Number(array.join('')));
+        }
+    })
+
+    const out = maxValue.length > 0 ? Math.max(...maxValue) : num;
+    console.log(out)
+    return out
+};
