4c2cec6eb69af7ef0d9950e9cdbd764545abe5ac
Sidharth T <sidhartht900@gmail.com>
2026-05-21 09:46:18 +0530

    Time: 113 ms (8.09%) | Memory: 57.8 MB (25.60%) - LeetSync

--- 268-missing-number/missing-number.js
+++ 268-missing-number/missing-number.js
@@ -4,15 +4,7 @@
  */
 var missingNumber = function(nums) {
     let n = nums.length;
-
-    // if(n === 1){
-    //     if(nums.includes(1)){
-    //         return 0;
-    //     }else{
-    //         return 1;
-    //     }
-    // }
-     
+         
      while(n){
         if(!nums.includes(n)){
          return n;
